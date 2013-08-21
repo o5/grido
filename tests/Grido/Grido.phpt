fdfac517a212e688e9ba251989dcef8c66da4ccb
Petr Bugyík <petr@bugyik.cz>
2013-08-21 15:57:16 +0200

    Initial commit...now just testing! :D

---
+++ tests/Grido/Grido.phpt
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/../bootstrap.php';
+
+test(function(){
+    Assert::same('TEST', 'TEST');
+});
---
+++ tests/bootstrap.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * Test initialization and helpers.
+ *
+ * @author     David Grudl
+ * @package    Nette\Test
+ */
+
+if (@!include __DIR__ . '/../vendor/autoload.php') {
+    echo 'Install Nette Tester using `composer update --dev`';
+    exit(1);
+}
+
+// configure environment
+Tester\Environment::setup();
+class_alias('Tester\Assert', 'Assert');
+date_default_timezone_set('Europe/Prague');
+
+
+// create temporary directory
+define('TEMP_DIR', __DIR__ . '/tmp/' . getmypid());
+@mkdir(dirname(TEMP_DIR)); // @ - directory may already exist
+Tester\Helpers::purge(TEMP_DIR);
+
+
+$_SERVER = array_intersect_key($_SERVER, array_flip(array('PHP_SELF', 'SCRIPT_NAME', 'SERVER_ADDR', 'SERVER_SOFTWARE', 'HTTP_HOST', 'DOCUMENT_ROOT', 'OS', 'argc', 'argv')));
+$_SERVER['REQUEST_TIME'] = 1234567890;
+$_ENV = $_GET = $_POST = array();
+
+
+if (extension_loaded('xdebug')) {
+    xdebug_disable();
+    Tester\CodeCoverage\Collector::start(__DIR__ . '/coverage.dat');
+}
+
+function id($val)
+{
+    return $val;
+}
+
+class Notes
+{
+    static public $notes = array();
+
+    public static function add($message)
+    {
+        self::$notes[] = $message;
+    }
+
+    public static function fetch()
+    {
+        $res = self::$notes;
+        self::$notes = array();
+        return $res;
+    }
+
+}
+
+function before(\Closure $function = NULL)
+{
+    static $val;
+    if (!func_num_args()) {
+        return ($val ? $val() : NULL);
+    }
+    $val = $function;
+}
+
+function test(\Closure $function)
+{
+    before();
+    $function();
+}
