a0d3357d322f3e5d8ff66b6b01c8c4912df25c09
Petr Bugyík <petr@bugyik.cz>
2013-09-02 02:22:31 +0200

    Added missing class

---
+++ Grido/Components/Columns/Text.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of the Grido (http://grido.bugyik.cz)
+ *
+ * Copyright (c) 2011 Petr Bugyík (http://petr.bugyik.cz)
+ *
+ * For the full copyright and license information, please view
+ * the file license.md that was distributed with this source code.
+ */
+
+namespace Grido\Components\Columns;
+
+/**
+ * Text column.
+ *
+ * @package     Grido
+ * @subpackage  Components\Columns
+ * @author      Petr Bugyík
+ */
+class Text extends Column {}
