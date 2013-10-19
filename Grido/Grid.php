d6a618eceab06ca877802c7846424d736a4f6adf
Petr Bugyík <petr@bugyik.cz>
2013-10-19 16:37:33 +0200

    Client side: Fixed multiple initialization [Close #79]

--- Grido/Grid.php
+++ Grido/Grid.php
@@ -537,7 +537,7 @@ class Grid extends \Nette\Application\UI\Control
         if ($this->tablePrototype === NULL) {
             $this->tablePrototype = \Nette\Utils\Html::el('table')
                 ->id($this->getName())
-                ->class('grido table table-striped table-hover');
+                ->class('table table-striped table-hover');
         }
 
         return $this->tablePrototype;
