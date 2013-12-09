007f53bc1a96639ce74ff2079dad37eebda23bee
Petr Bugyík <petr@bugyik.cz>
2013-12-09 14:24:40 +0100

    Filter\Date: Added building of default date-condition

--- Grido/Components/Filters/Date.php
+++ Grido/Components/Filters/Date.php
@@ -17,14 +17,69 @@ namespace Grido\Components\Filters;
  * @package     Grido
  * @subpackage  Components\Filters
  * @author      Petr Bugyík
+ *
+ * @property string $dateFormatInput
+ * @property string $dateFormatOutput
  */
 class Date extends Text
 {
+    const DEFAULT_CONDITION = '= ?';
+
     /** @var string */
-    protected $condition = '= ?';
+    protected $condition = self::DEFAULT_CONDITION;
 
     /** @var string */
     protected $formatValue;
+
+    /**
+     * @var string
+     */
+    protected $dateFormatInput = 'd.m.Y';
+
+    /**
+     * @var string
+     */
+    protected $dateFormatOutput = 'Y-m-d';
+
+    /**
+     * Sets date-input format.
+     * @param string $format
+     * @return Date
+     */
+    public function setDateFormatInput($format)
+    {
+        $this->dateFormatInput = $format;
+        return $this;
+    }
+
+    /**
+     * Returns date-input format.
+     * @return string
+     */
+    public function getDateFormatInput()
+    {
+        return $this->dateFormatInput;
+    }
+
+    /**
+     * Sets date-output format.
+     * @param string $format
+     * @return Date
+     */
+    public function setDateFormatOutput($format)
+    {
+        $this->dateFormatOutput = $format;
+        return $this;
+    }
+
+    /**
+     * Returns date-output format.
+     * @return string
+     */
+    public function getDateFormatOutput()
+    {
+        return $this->dateFormatOutput;
+    }
 
     /**
      * @return \Nette\Forms\Controls\TextInput
@@ -36,5 +91,22 @@ class Date extends Text
         $control->getControlPrototype()->attrs['autocomplete'] = 'off';
 
         return $control;
+    }
+
+    /**
+     * @param string $value
+     * @return Condition
+     * @throws \Exception
+     * @internal
+     */
+    public function __getCondition($value)
+    {
+        if ($this->where === NULL && $this->condition == self::DEFAULT_CONDITION) {
+            return ($date = \DateTime::createFromFormat($this->dateFormatInput, $value))
+                ? Condition::setupFromArray(array($this->getColumn(), $this->condition, $date->format($this->dateFormatOutput)))
+                : Condition::setupEmpty();
+        }
+
+        return parent::__getCondition($value);
     }
 }
--- tests/Grido/Components/Filter.Date.phpt
+++ tests/Grido/Components/Filter.Date.phpt
@@ -29,7 +29,16 @@ class FilterDateTest extends \Tester\TestCase
     {
         $grid = new Grid;
         $filter = $grid->addFilterDate('date', 'Date');
-        Assert::same(array('date = ?', 'TEST'), $filter->__getCondition('TEST')->__toArray());
+
+        Assert::same(array('date = ?', '2012-12-21'), $filter->__getCondition('21.12.2012')->__toArray());
+        Assert::same(array('0 = 1'), $filter->__getCondition('TEST BAD INPUT')->__toArray());
+
+        $filter
+            ->setDateFormatInput('d/m/Y')
+            ->setDateFormatOutput('d.m.Y');
+
+        Assert::same(array('date = ?', '21.12.2012'), $filter->__getCondition('21/12/2012')->__toArray());
+        Assert::same(array('0 = 1'), $filter->__getCondition('21.12.2012')->__toArray()); //test bad input
     }
 }
 
