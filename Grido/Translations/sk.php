868214560c1555d1c751bff9222674b8fd8fcc7c
Kollarovic <kollarovic@gmail.com>
2014-01-12 09:20:10 +0100

    Add sk translation

---
+++ Grido/Translations/sk.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of the Grido (http://grido.bugyik.cz)
+ *
+ * Copyright (c) 2011 Petr Bugyík (http://petr.bugyik.cz)
+ *
+ * For the full copyright and license information, please view
+ * the file LICENSE.md that was distributed with this source code.
+ */
+
+return array(
+    'You can use <, <=, >, >=, <>. e.g. ">= %d"' => 'Môžete použiť <, <=, >, >=, <>. Napr.: ">= %d"',
+    'Select some row' => 'Vyberte riadok',
+    'Invert' => 'Obrátit výber',
+    'Items %d - %d of %d' => 'Položky %d - %d z %d',
+    'Previous' => 'Predchádzajúci',
+    'Next' => 'Ďalší',
+    'Actions' => 'Akcie',
+    'Search' => 'Vyhľadať',
+    'Reset' => 'Zrušiť',
+    'Items per page' => 'Položiek na stránku',
+    'Selected...' => 'Vybrané...',
+    'Enter page:' => 'Vložte stranu:',
+    'No results.' => 'Žiadne výsledky.',
+    'Export all items' => 'Exportovať všetky položky',
+);
