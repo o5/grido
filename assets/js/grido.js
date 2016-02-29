8bacf40e92eeac4774ee827068902e36add18dde
Ciki <matula.m@gmail.com>
2016-02-29 22:39:20 +0100

    fix history api
    handle only own history stack & make history api actually work

--- assets/js/grido.js
+++ assets/js/grido.js
@@ -479,6 +479,8 @@
          */
         onSuccessEvent: function(params, url)
         {
+            params.url = url;
+            params.grido = true;
             window.history.pushState(params, document.title, url);
         },
 
@@ -487,12 +489,9 @@
          */
         onPopState: function(event)
         {
-            var state = $.data(document, this.grido.name + '-query') || '',
-                query = window.location.search;
-
-            if (state !== query) {
-                var url = this.getRefreshGridHandlerUrl(this.grido.$element);
-                this.doRequest(url);
+            var state = event.originalEvent.state;
+            if (state.grido) {
+                this.doRequest(state.url);
             }
         },
 
